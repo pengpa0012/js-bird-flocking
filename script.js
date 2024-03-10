882cf796cb66e029d18198d86817a919dccce2f6
Pengpa0012 <peng.ilikha@gmail.com>
2024-03-11 07:42:53 +0800

    setup initial canvas

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 const canvas = document.querySelector("canvas")
 const ctx = canvas.getContext("2d")
+
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+function drawCenteredRectangle() {
+  const rectWidth = 150
+  const rectHeight = 75
+  const x = (canvas.width - rectWidth) / 2
+  const y = (canvas.height - rectHeight) / 2
+
+  ctx.fillRect(x, y, rectWidth, rectHeight)
+}
+
+drawCenteredRectangle()
+
+window.addEventListener("resize", () => {
+  canvas.width = window.innerWidth
+  canvas.height = window.innerHeight
+  drawCenteredRectangle()
+})
