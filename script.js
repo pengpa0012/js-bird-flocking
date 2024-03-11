a2bf25b4e0bdf20196601ca64f786d5673763333
Pengpa0012 <peng.ilikha@gmail.com>
2024-03-11 20:18:42 +0800

    added moving straight line

--- script.js
+++ script.js
@@ -4,19 +4,23 @@ const ctx = canvas.getContext("2d")
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
-function drawCenteredRectangle() {
-  const rectWidth = 150
-  const rectHeight = 75
-  const x = (canvas.width - rectWidth) / 2
-  const y = (canvas.height - rectHeight) / 2
-
-  ctx.fillRect(x, y, rectWidth, rectHeight)
+const SPEED = 5
+const pos = {
+  x: 0,
+  y: 100
 }
 
-drawCenteredRectangle()
+function draw() {
+  const rectWidth = 10
+  const rectHeight = 2
+  pos.x += SPEED
+  ctx.fillRect(pos.x, pos.y, rectWidth, rectHeight)
+}
 
-window.addEventListener("resize", () => {
-  canvas.width = window.innerWidth
-  canvas.height = window.innerHeight
-  drawCenteredRectangle()
-})
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  requestAnimationFrame(animate)
+  draw()
+}
+
+animate()
