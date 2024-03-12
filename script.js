e47369ca6976d0884cda925702a04af7fcfb0a56
Pengpa0012 <peng.ilikha@gmail.com>
2024-03-12 08:28:20 +0800

    added limit on x-axis

--- script.js
+++ script.js
@@ -7,13 +7,28 @@ canvas.height = window.innerHeight
 const SPEED = 5
 const pos = {
   x: 0,
-  y: 100
+  y: Math.floor(Math.random() * canvas.height),
+  direction: 1
 }
 
 function draw() {
   const rectWidth = 10
   const rectHeight = 2
-  pos.x += SPEED
+
+  if (pos.direction === 1) {
+    pos.x += SPEED
+  } else {
+    pos.x -= SPEED
+  }
+
+  if (pos.x + rectWidth >= canvas.width) {
+    pos.direction = -1
+  }
+
+  if (pos.x <= 0) {
+    pos.direction = 1 
+  }
+
   ctx.fillRect(pos.x, pos.y, rectWidth, rectHeight)
 }
 
